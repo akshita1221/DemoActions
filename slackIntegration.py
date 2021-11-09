7d2d827485aac4cb5e04b9fee435d278f1908ff7
akshita1221 <86468699+akshita1221@users.noreply.github.com>
2021-11-09 22:54:17 +0530

    Add files via upload

---
+++ slackIntegration.py
@@ -0,0 +1,35 @@
+import json
+from os import read
+import requests
+import sys
+
+token_var = sys.argv[1]
+fileName = "DemoActions.pdf"
+filePath = "./"+fileName
+
+def send_report_to_slack(filePath,fileName):
+    url = "https://slack.com/api/files.upload"
+
+    #reading file in binary form
+    with open(filePath, "rb") as f:
+        read_data = f.read()
+
+    data = {
+        "token": token_var,
+        "channels": "",
+        "content": read_data,
+        "filename": fileName,
+        "title": "Demo test Actions slack test result: "+fileName,
+        "initial_comment": "Demo Actions testing report",
+    }
+
+    requests.post(
+        url=url, data=data,
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
+
+if token_var != "":
+    send_report_to_slack(filePath,fileName)
+else:
+    print("slack token value is required")
+    raise Exception
